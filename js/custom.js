e3e22c1f858285686291dfe30564697ae36b9f3e
Harrison Peace <harrison@apwines.com>
2021-03-22 20:12:09 +1100

    added width for video container

--- js/custom.js
+++ js/custom.js
@@ -82,10 +82,10 @@ function touchMoveGreyscaleImg (e) {
 					enableScroll();
 					greyscaleProfileImg.style.transition = 'clip-path .3s ease-out';
 				} else if ((ScrollDirection) == -1 || (ScrollDirection) == 1) {
-						scrollGreyscaleImg(ScrollDirection * 5);
+						scrollGreyscaleImg(ScrollDirection * 4);
 				}
 			} else if ((ScrollDirection) == -1 || (ScrollDirection) == 1) {
-						scrollGreyscaleImg(ScrollDirection * 5);
+						scrollGreyscaleImg(ScrollDirection * 4);
 			}
 		}
 	previousPosition = currentPosition;
