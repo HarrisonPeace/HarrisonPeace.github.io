12ef859a7d75cc2579ee664ed05b88e48c661ba2
Harrison Peace <harrison@apwines.com>
2021-03-22 15:48:00 +1100

    Changhed scroll functionailty

--- js/custom.js
+++ js/custom.js
@@ -97,10 +97,12 @@ function touchMoveGreyscaleImg (e) {
 					enableScroll();
 					greyscaleProfileImg.style.transition = 'clip-path .3s ease-out';
 				} else if ((ScrollDirection) == -1 || (ScrollDirection) == 1) {
-						scrollGreyscaleImg(ScrollDirection);
+						scrollGreyscaleImg(ScrollDirection * 2);
+						console.log(ScrollDirection * 2)
 				}
 			} else if ((ScrollDirection) == -1 || (ScrollDirection) == 1) {
-						scrollGreyscaleImg(ScrollDirection);
+						scrollGreyscaleImg(ScrollDirection * 2);
+						console.log(ScrollDirection * 2)
 			}
 		}
 	previousPosition = currentPosition;
